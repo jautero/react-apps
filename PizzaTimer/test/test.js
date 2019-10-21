65e940639928e93875db961be887bfe4b8335d1c
Juha Autero <jautero@iki.fi>
2019-10-21 14:02:27 +0300

    Working babel and import.

--- PizzaTimer/test/test.js
+++ PizzaTimer/test/test.js
@@ -1,6 +1,10 @@
-var assert = require('assert');
+import {strict as assert } from 'assert';
+import {text} from '../PizzaTimer.js';
+
 describe('Basic Timer tests',function() {
     describe('Timer result',function() {
-        assert.fail("Failing test")
+        it("Should have text",function() {
+            assert(text());     
+        })
     })
 })
